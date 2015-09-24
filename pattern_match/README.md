20de75fbff4d1759412026d3ad516b1760b73f3c
Yuichi Murata <mrk21info+github@gmail.com>
2015-09-24 23:35:20 +0900

    pattern_match: add main usage

--- pattern_match/README.md
+++ pattern_match/README.md
@@ -10,3 +10,4 @@ $ elixir main.exs
 
 * [Elixirだ 第1回 - 基礎だ -](http://www.slideshare.net/Joe_noh/elixir-01)
 * [4 パターンマッチング - Pattern matching - Elixir](http://elixir-ja.sena-net.works/getting_started/4.html#4.3-ピン演算子---the-pin-operator)
+* [Elixir Exception と try-rescue - Qiita](http://qiita.com/tbpgr/items/088e134b2d83c7a4eea7)
--- pattern_match/main.exs
+++ pattern_match/main.exs
@@ -16,8 +16,35 @@ IO.inspect [y, _m, _d]
 IO.puts ""
 
 
-IO.puts "## PinOperator"
-x = 1
-[^x, y, 3] = [1, 2, 3]
-#[^x, y, 3] = [5, 2, 3] # MatchError
+IO.puts "## Pin operator"
+try do
+  x = 1
+  [^x, y, 3] = [1, 2, 3]
+  [^x, y, 3] = [5, 2, 3]
+rescue
+  e in MatchError -> IO.inspect e
+end
+IO.puts ""
+
+
+IO.puts "## Main usage"
+{:error, message} = File.read 'not_existed_file.txt'
+IO.inspect message # :enoent
+
+try do
+  {:ok, res} = File.read 'not_exlisted_file.txt'
+rescue
+  e in MatchError -> IO.inspect e
+end
+
+[head | tail] = [1,2,3,4]
+IO.inspect head
+IO.inspect tail
+
+IO.inspect(case File.read 'not_existed_file.txt' do
+  {:ok, res} -> res
+  {:error, :enoent} -> 'not existed'
+  {:error, :eacces} -> 'can not read'
+end)
+
 IO.puts ""
