63ac286e28aa4b0cacd85b30f4667a876c47ca91
Yuichi Murata <mrk21info+github@gmail.com>
2015-09-25 23:21:53 +0900

    control_syntax: add a behavior of the case when not matched

--- control_syntax/main.exs
+++ control_syntax/main.exs
@@ -3,6 +3,14 @@ IO.inspect(case 2 do
   1 -> "one"
   2 -> "two"
 end)
+try do
+  IO.inspect(case 3 do
+    1 -> "one"
+    2 -> "two"
+  end)
+rescue
+  e in CaseClauseError -> IO.inspect e
+end
 IO.puts ""
 
 
@@ -20,4 +28,12 @@ IO.inspect(cond do
   2 == 1 -> "one"
   2 == 2 -> "two"
 end)
+try do
+  IO.inspect(cond do
+    2 == 1 -> "one"
+    3 == 2 -> "two"
+  end)
+rescue
+  e in CondClauseError -> IO.inspect e
+end
 IO.puts ""
