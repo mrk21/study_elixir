aabef2ee6481931dc96334bcc9246290702ee601
Yuichi Murata <mrk21info+github@gmail.com>
2015-10-07 09:07:44 +0900

    meta_promramming: add a code of macro

--- meta_programming/main.exs
+++ meta_programming/main.exs
@@ -5,3 +5,30 @@ IO.inspect(ast = quote do
 end)
 IO.inspect(Code.eval_quoted ast)
 IO.puts ''
+
+
+IO.puts '## Macro'
+defmodule MacroModule do
+  defmacro value(ast) do
+    ast
+  end
+  
+  defmacro value_impl(ast) do
+    ast
+  end
+  
+  def call do
+    value(IO.puts("1"))
+  end
+end
+MacroModule.call
+
+defmodule UsingMacroModule do
+  require MacroModule
+  
+  def use_macro do
+    MacroModule.value(IO.puts("2"))
+  end
+end
+UsingMacroModule.use_macro
+IO.puts ''
