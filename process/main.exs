41f65499588a293688bf13e6bb08bdf8999883dd
Yuichi Murata <mrk21info+github@gmail.com>
2015-10-11 23:56:33 +0900

    process: add a code of config process

--- process/main.exs
+++ process/main.exs
@@ -1,6 +1,35 @@
+IO.puts "## Base"
 send self(), {:hello, "world"}
 
 IO.inspect(receive do
   {:hello, msg} -> msg
   {:world, _} -> "won't match"
 end)
+IO.puts ""
+
+
+IO.puts "## Config Process"
+defmodule KV do
+  def start_link do
+    {:ok, spawn_link(fn -> loop(%{}) end)}
+  end
+  
+  defp loop(map) do
+    receive do
+      {:get, key, caller} ->
+        send caller, Map.get(map, key)
+        loop(map)
+      {:put, key, value} ->
+        loop(Map.put(map, key, value))
+    end
+  end
+end
+
+IO.inspect {:ok, pid} = KV.start_link
+IO.inspect send pid, {:put, :hello, :world}
+IO.inspect send pid, {:get, :hello, self()}
+
+receive do
+  value -> IO.inspect value
+end
+IO.puts ""
