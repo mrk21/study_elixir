8b5f4ea8e94785f109801354eb10a4f71522de59
Yuichi Murata <mrk21info+github@gmail.com>
2015-10-12 23:56:33 +0900

    process: add a code of OTP

--- process/main.exs
+++ process/main.exs
@@ -33,3 +33,25 @@ receive do
   value -> IO.inspect value
 end
 IO.puts ""
+
+
+IO.puts "## OTP"
+defmodule GenServerSample do
+  use GenServer
+  
+  def start_link do
+    GenServer.start_link(__MODULE__, 0)
+  end
+  
+  def next(pid) do
+    GenServer.call(pid, :next)
+  end
+  
+  def handle_call(:next, _from, current) do
+    {:reply, current, current+1}
+  end
+end
+IO.inspect {:ok, pid} = GenServerSample.start_link
+IO.inspect GenServerSample.next(pid)
+IO.inspect GenServerSample.next(pid)
+IO.puts ""
